cc61a167c5b7f4f4b0f67779eed2545280821e1a
Wafiki789 <raphaelle.mp@gmail.com>
2016-05-18 16:58:06 -0400

    Negative binary numbers
    There is an obvious way to factorized it.

--- NewMain.java
+++ NewMain.java
@@ -1,4 +1,3 @@
-//Maybe TODO: Negative numbers?
 package nc;
 
 import java.io.BufferedReader;
@@ -15,7 +14,6 @@ public class NewMain {
         }catch (IOException e){
             System.out.println("Ce n'est pas un nombre.");
             return 0;
-            //Eventually, put a boolean?
         }
     }
     
@@ -24,22 +22,33 @@ public class NewMain {
                     + "\nbase 16 and base 8. Enter 0 to exit.");
         while(true){
             int number = 1;
+            boolean neg = false;
+            boolean error = false;
             try{
                 number = readInt();
-            }catch (NumberFormatException e){
-                System.out.println("Ce n'est pas un nombre.");
+            }catch (NumberFormatException e){ //bool?
+                System.out.println("Error: not a number.");
+                error = true;
             }
 
-            if(number != 0){
+            if(!error && number != 0){
+                if(number < 0){
+                    number *= -1;
+                    neg = true;
+                }
                 String binaryNum = conversion(number, 2);
                 String hexaNum = conversion(number, 16);
                 String octalNum = conversion(number, 8);
+                
+                if(neg){
+                    binaryNum = binNegative(binaryNum);
+                }
                 System.out.println("Binary: " + binaryNum +
                                    "\nHexadecimal: " + hexaNum + 
                                    "\nOctal: " + octalNum +
                                    "\nEnter a new number: ");
             }
-            else{
+            else if(!error){
                 System.exit(0);
             }
         }
@@ -88,5 +97,72 @@ public class NewMain {
             convertedNum += temp.charAt(i);
         }
         return convertedNum;
+    }
+    
+    public static String binNegative(String posBin){
+        String negBin = "";
+        
+        if(posBin.length() != 16){
+            String temp1 = "";
+            for(int i = 0; i < 16; i++){
+                if(posBin.length() + i < 16){
+                    temp1 += '0';
+                }
+                else{
+                    break;
+                }
+            }
+            
+            for(int i = 0; i < posBin.length(); i++){
+                temp1 += posBin.charAt(i);
+            }
+            
+            posBin = temp1;
+        }
+        
+        for(int i = 0; i < posBin.length(); i++){
+            if(posBin.charAt(i) == '1'){
+                negBin += 0;
+            }
+            else{
+                negBin += 1;
+            }
+        }
+        
+        if(negBin.charAt(negBin.length()-1) == '0'){
+            String temp2 = "";
+            for(int i = 0; i < negBin.length()-1; i++){
+                temp2 += negBin.charAt(i);
+            }
+            temp2 += '1';
+            negBin = temp2;
+        }
+        else{
+            String temp3 = "";
+            int index = 0;
+            for(int i = negBin.length()-1; i >= 0; i--){
+                if(negBin.charAt(i) == '0'){
+                    index = i;
+                    break;
+                }
+            }
+            
+            for(int i = 0; i < negBin.length() - 1; i++){
+                if(i == index){
+                    temp3 += '1';
+                    break;
+                }
+                else{
+                    temp3 += negBin.charAt(i);
+                }
+                System.out.println("Temp: " + temp3);
+            }
+            for(int i = index +1; i < negBin.length(); i++){
+                temp3 += '0';
+            }
+            negBin = temp3;
+        }
+        
+        return negBin;
     }
 }
