803bdc83ab2ced9190ff989ac116ed595e27b4aa
Wafiki789 <raphaelle.mp@gmail.com>
2016-05-17 23:17:21 -0400

    Next: Negative numbers
    It works with hexa, octal and binary! Wow!

--- NewMain.java
+++ NewMain.java
@@ -20,30 +20,66 @@ public class NewMain {
     }
     
     public static void main(String[] args) {
-        int number;
         System.out.println("Enter a decimal integer to see its equivalent in base 2,"
-                + "\nbase 16 and base 8. Enter 0 to exit.");
-        number = readInt();
-        if(number != 0){
-            int binaryNum = binaryConv(number);
-            //int hexaNum = hexaConv(number);
-            //int octalNum = octalConv(number);
-            System.out.println("Binary: " + binaryNum);
+                    + "\nbase 16 and base 8. Enter 0 to exit.");
+        while(true){
+            int number = 1;
+            try{
+                number = readInt();
+            }catch (NumberFormatException e){
+                System.out.println("Ce n'est pas un nombre.");
+            }
+
+            if(number != 0){
+                String binaryNum = binaryConv(number, 2);
+                String hexaNum = binaryConv(number, 16);
+                String octalNum = binaryConv(number, 8);
+                System.out.println("Binary: " + binaryNum +
+                                   "\nHexadecimal: " + hexaNum + 
+                                   "\nOctal: " + octalNum +
+                                   "\nEnter a new number: ");
+            }
+            else{
+                System.exit(0);
+            }
         }
-        else{
-            number = -1;
-        }
-        //System.exit(0);
     }
     
     //Try to implement this with a while, then with recursion.
-    public static int binaryConv(int number){
+    public static String binaryConv(int number, int base){
         String binaryNum = "";
         int remainder = 0;
+        char digit;
         while(number != 0){
-            remainder = number % 2;
-            number /= 2;
-            binaryNum += remainder;
+            remainder = number % base;
+            number /= base;
+            if(base == 16){
+                if(remainder == 15){
+                    digit = 'F';
+                }
+                else if(remainder == 14){
+                    digit = 'E';
+                }
+                else if(remainder == 13){
+                    digit = 'D';
+                }
+                else if(remainder == 12){
+                    digit = 'C';
+                }
+                else if(remainder == 11){
+                    digit = 'B';
+                }
+                else if(remainder == 10){
+                    digit = 'A';
+                }
+                else{
+                    digit = (char)('0' + remainder);
+                }
+            }
+            else{
+                digit = (char)('0' + remainder);
+            }
+            binaryNum += digit;
         }
         
         //Reverse
@@ -51,6 +87,10 @@ public class NewMain {
         for(int i = binaryNum.length()-1; i >= 0; i--){
             temp += binaryNum.charAt(i);
         }
-        return Integer.parseInt(binaryNum);
+        return temp;
+    }
+    
+    public static int hexaConv(int number){
+        return 1;
     }
 }
